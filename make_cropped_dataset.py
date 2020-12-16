941a4397f8efaec785e2e5ff40bae8d6de1617bf
sin392@gmail.com <sin392@gmail.com>
2020-12-17 00:23:08 +0900

    update

--- make_cropped_dataset.py
+++ make_cropped_dataset.py
@@ -9,8 +9,8 @@ import torch
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--input', '-i', default='./data/test', type=str)
-parser.add_argument('--batch', '-b', default=15, type=int)
+parser.add_argument('--input', '-i', default='./data/train', type=str)
+parser.add_argument('--batch', '-b', default=150, type=int)
 parser.add_argument('--size', '-s', default=224, type=int)
 
 args = parser.parse_args()
@@ -19,14 +19,14 @@ batch_size = args.batch
 image_size = args.size
 
 mode = os.path.basename(inp_dir)
-if os.path.exists(f"{mode}_list.txt"):
-    print(f"use {mode}_list.txt")
+if os.path.exists(f'{mode}_list.txt'):
+    print(f'use {mode}_list.txt')
     with open(f"{mode}_list.txt", mode="rt") as f:
-        actual_list = [x.rstrip() for x in f.readlines()]
-    # WARN : ファイル欠損してる可能性
-    img_paths = [os.path.join(inp_dir, x) for x in actual_list]
+        img_paths = [os.path.join(inp_dir, x.rstrip()) for x in f.readlines()]
 else:
+    print('glob mode')
     img_paths = glob(os.path.join(inp_dir, '*/*'))
+
 
 print('input_dir :', inp_dir)
 print('batch_size :', batch_size)
@@ -38,11 +38,13 @@ print('device :', device)
 model = MTCNN(image_size=image_size, select_largest=True,
               post_process=False, device=device)
 
-for i in tqdm(range(0, len(img_paths), batch_size)):
+for loop, i in enumerate(tqdm(range(0, len(img_paths), batch_size))):
+    if loop < 805:
+        continue
     if i+batch_size <= len(img_paths):
         batch_paths = img_paths[i:i+batch_size]
     else:
-        batch_paths = img_paths[i:i+(len(batch_size - batch_size))]
+        batch_paths = img_paths[i:]
     batch_img = [Image.open(path).resize((image_size, image_size))
                  for path in batch_paths if os.path.exists(path)]
     save_paths = [path.replace('data', 'processed_data')
